594c1b321a2c4135b5c16da382e319a206b002d7
임형대 <imhyeongdae@imhyeongdaeui-MacBook-Pro.local>
2019-08-13 21:32:14 +0900

    Oauth complete

--- src/api/twitterimage/twitterimage.js
+++ src/api/twitterimage/twitterimage.js
@@ -1,9 +1,10 @@
-import { searchTopic } from "../../twitterapi";
+import { searchTopic , searchUser } from "../../twitterapi";
 
 //console.log(searchTopic('twice',1234));
 
 export default {
     Query: {
+        searchU:(_, { q }) => searchUser(q),
         search:(_, { q,count }) => searchTopic(q,count)
       }
   };
--- src/twitterapi.js
+++ src/twitterapi.js
@@ -1,77 +1,6 @@
-import Twitter from 'twitter';
 import axios from 'axios';
-import twitterimage from './api/twitterimage/twitterimage';
+import oauth from 'oauth';  
 
-
-  /*
-  const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
-
-const params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
-    }
-  });
-  
- client.get('search/tweets', {q: 'twice'}, function(error, tweets, response) {
-  //console.log(tweets);
-  //data = data.concat(tweets);
-});
-
-
-const gettwitt = () => {
-  client.stream('statuses/filter', {track: '사나'},  function(stream) {
-      stream.on('data', function(tweet) {
-        //console.log(tweet.entities.media);
-      });
-    
-      stream.on('error', function(error) {
-        console.log(error);
-      });
-      return "test";
-    });
-};
-
-let data = [];
-const tweets = (params, max, callback) => {
-  client.get('statuses/user_timeline', params, (error, results, response) => {
-    if (error) {
-      return callback(error);
-    }
-    
-    data = data.concat(results);
-    
-    if(data.length <= max) {
-      params.max_id = results.pop().id;
-
-      return tweets(params, max, callback);
-    }
-    
-    return callback(error, data, response);
-  });
-}
-
-tweets({screen_name: 'jack', count: 10}, 50, (error, tweets, response) => {
-  if (!error) {
-    //console.log(tweets);
-  }
-});
-
-const te = () => {
-  console.log("Test");
-  return "test is work";
-};
-
-const cube = (x) => {
-  return x * x * x;
-}
-const foo = Math.PI + Math.SQRT2;
-*/
 const USER_TOKEN = process.env.TWITTER_ACCESS_BEARER_TOKEN;  
 
   const consumer_key = process.env.TWITTER_CONSUMER_KEY;
@@ -79,6 +8,28 @@ const USER_TOKEN = process.env.TWITTER_ACCESS_BEARER_TOKEN;
   const access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY;
   const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
 
+  function getAuthorization(httpMethod, baseUrl, reqParams) {
+    
+    // Get acces keys
+    const consumerKey       = process.env.TWITTER_CONSUMER_KEY,
+        consumerSecret      = process.env.TWITTER_CONSUMER_SECRET,
+        accessToken         = process.env.TWITTER_ACCESS_TOKEN_KEY,
+        accessTokenSecret   = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+  
+    //헤더파일 인증 생성
+    var consumer = new oauth.OAuth(
+      "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token", 
+      consumerKey ,  consumerSecret, "1.0A", "http://127.0.0.1:8080/sessions/callback", "HMAC-SHA1");
+    var parameters2 = consumer._prepareParameters(accessToken, accessTokenSecret, httpMethod, baseUrl);
+    var headers = consumer._buildAuthorizationHeaders(parameters2);
+   
+    console.log(headers);  
+    return headers;
+   
+    }
+
+
+  //test 필요
 export const tweetStream = async (text) => {
 
   const baseURL = "https://stream.twitter.com/1.1/statuses/filter.json";
@@ -88,17 +39,39 @@ export const tweetStream = async (text) => {
 
   const tweetRequest =  await axios(URL, { 
       headers: { Authorization: 
-      'OAuth oauth_consumer_key='+ consumer_key +
-      'oauth_nonce='+consumer_secret +
-      'oauth_signature=' + USER_TOKEN +
+      'OAuth oauth_consumer_key='+ consumer_key + '", ' +
+      'oauth_nonce='+consumer_secret + '", ' +
+      'oauth_signature=' + USER_TOKEN + '", ' +
       'oauth_signature_method="HMAC-SHA1",' +
       'oauth_version="1.0",' +
-      'oauth_token='+ access_token_key 
+      'oauth_token='+ access_token_key + '",'
       } 
     });
   const tdata = tweetRequest;
   console.log(Authorization);
   //console.log(tdata);
+  return tdata;
+};
+
+
+export const searchUser = async (text) => {
+  
+  const baseURL = "https://api.twitter.com/1.1/users/search.json";
+  
+  const searchParams = `?q=${text}`;
+  const URL = baseURL + searchParams;
+  
+  const tweetRequest =  await axios(URL, { 
+    headers: {
+      Authorization: getAuthorization(
+          'GET',
+          URL
+      )
+  }
+    });
+    console.log(tdata)  
+  const tdata = tweetRequest.data;
+  console.log(tdata)
   return tdata;
 };
 
@@ -116,13 +89,16 @@ export const searchTopic = async (text,count) => {
       headers: { Authorization: AuthStr }
     });
   const tdata = tweetRequest.data.statuses;
+  //console.log(AuthStr)
   return tdata;
 };
 
-console.log(tweetStream('twice'));
-//console.log(searchTopic('twice'));
+/*
+console.log(getAuthorization(
+  'GET',
+  'https://api.twitter.com/1.1/users/search.json?q=twice',
+));
+*/
+console.log(searchUser('BTS'));
+//onsole.log(searchTopic('twice'));
 
-
-
-
-    
